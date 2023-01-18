a82b9951ac14073df24ef9dbe125be1e9699a043
ninayeh <itsninayeh@gmail.com>
2023-01-18 17:42:16 +0900

    Add My Portfolio content

--- src/js/main.js
+++ src/js/main.js
@@ -3,3 +3,8 @@ import '../scss/styles.scss'
 
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap'
+
+import '@fortawesome/fontawesome-free/js/fontawesome'
+import '@fortawesome/fontawesome-free/js/solid'
+import '@fortawesome/fontawesome-free/js/brands'
+import '@fortawesome/fontawesome-free/js/regular'
